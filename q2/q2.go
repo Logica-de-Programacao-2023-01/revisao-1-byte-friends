582e78151c96eb3df738f0a931ca0ce9ac7e999b
Giovana Amorim Campos <giovana.amorimc@dinfor.uniceub>
2023-04-18 10:48:17 -0300

    q2

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,32 @@
 package q2
 
+import (
+	"fmt"
+	"strings"
+)
+
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	if len(text) == 0 {
+		return 0, fmt.Errorf("texto vazio")
+	}
+	// limpagem
+	slice := []string{""}
+	text = strings.ReplaceAll(text, ",", "")
+	text = strings.ReplaceAll(text, "!", "")
+
+	// separar as palavras e coloca em uma slice
+	slice = strings.Fields(text)
+	var words float64
+	words = float64(len(slice))
+
+	//quantidade de letras
+	var letras float64
+	text = strings.ReplaceAll(text, " ", "")
+	letras = float64(len(text)) - 2
+	fmt.Println(text, letras, words)
+
+	var media float64
+	media = letras / words
+
+	return media, nil
 }
